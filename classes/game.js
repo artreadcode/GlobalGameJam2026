3cb221b3a16e08ea70a48f48b904096ebd65abdb
Astryd Park <artreadcode@gmail.com>
2026-01-31 11:39:27 +0000

    Set up the basic template
    1. Load the ml5.js model and its basic callback function
    2. Handle the windowResize event correctly
    3. Made the `Game` class for OOP

---
+++ classes/game.js
@@ -0,0 +1,50 @@
+class Game {
+    constructor() {
+        this.started = false;
+        this.ended = false;
+
+        this.w = windowWidth;
+        this.h = windowHeight;
+
+        // For the current stage; Where are you?
+        this.stage = 0;
+
+        // To store what was the previous stage
+        this.prev = 0;
+
+        // Shall we move on? (Triggered from each scene)
+        this.next = false;
+
+        // For each play
+        this.play;
+
+        this.background = 0;
+    }
+
+    show() {
+        createCanvas(this.w, this.h);
+        background(this.background);
+        switch (this.scene) {
+            case 0: // Start screen
+                if (!this.started) {
+                    this.started = true;
+                    
+                }
+                else {
+                    this.play.show();
+                }
+
+                if (this.next) {
+                    this.scene = 1; // Move onto the next stage.
+                    this.ended = true;
+                }
+                break;
+
+            case 1: // Stage 01
+            // Same structure will happen here...
+            case 2: // Stage 02
+            case 3: // Stage 03
+            case 4: // Ending scene
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,47 @@
+// Hold the ml5.js FaceMesh model
+// We need to use FaceMesh > Facial Landmark Detection feature.
+let faceMesh;
+let video; // The video to store the webcam video.
+let faces = []; // This one will store the markers across the faces.
+let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
+
+function preload() {
+  // Load the FaceMesh model
+  faceMesh = ml5.faceMesh(options);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  console.log('Global Game Jam 2026 project');
+  // Create the game
+  game = new Game();
+  // Create the gameplay window
+  // createCanvas(windowWidth, windowHeight);
+  // Build the Video object, preparing for the transition screen(s).
+
+  // *** CAUTION: It will trigger the inline popup to ask for permission from the camera.
+  video = createCapture(VIDEO);
+  // Set up the video size with video.size(sth, sth)
+  video.hide(); // Because we won't show this video right away.
+
+  // c.f. If you want to start detecting faces from the webcam video
+  // faceMesh.detectStart(video, gotFaces);
+  // If you don't 'draw' a video, it won't display the webcam video on the screen so FYI.
 }
 
 function draw() {
-  background(220);
+  game.show();
+  // background(0); // Just set up the thing as black
+}
+
+// Handle window resizing event so our game won't be seen weird.
+// Ref: https://p5js.org/reference/p5/windowResized/
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// Callback function for when faceMesh outputs data
+function gotFaces(results) {
+  // Save the output to the faces variable
+  faces = results;
+  // And let's add something here...
 }
